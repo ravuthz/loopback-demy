9fca1777f4d135d487e32b226e97565ac4dbab11
Ravuthz <ravuthz@gmail.com>
2018-09-02 00:42:50 +0700

    Add customer end points - lb remote-method

--- common/models/member.js
+++ common/models/member.js
@@ -2,4 +2,16 @@
 
 module.exports = function(Member) {
 
+  /**
+   * takes in a name and then says it
+   * @param {string} myName Chuck in the name of the person
+   * @param {Function(Error, object)} callback
+   */
+
+  Member.sayMyName = function(myName, callback) {
+    var myResponse = 'Your name is ' + myName;
+    // TODO
+    callback(null, myResponse);
+  };
+
 };
